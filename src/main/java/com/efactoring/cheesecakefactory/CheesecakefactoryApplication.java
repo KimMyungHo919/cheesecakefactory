4b06e9cb245b1c4b8e6d7907ddb263a77481577e
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2024-12-03 14:08:56 +0900

    First commit

---
+++ src/main/java/com/efactoring/cheesecakefactory/CheesecakefactoryApplication.java
@@ -0,0 +1,13 @@
+package com.efactoring.cheesecakefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheesecakefactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheesecakefactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/efactoring/cheesecakefactory/CheesecakefactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efactoring.cheesecakefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheesecakefactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
