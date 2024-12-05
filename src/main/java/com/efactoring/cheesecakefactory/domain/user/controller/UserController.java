19cbda34d999c3fcb8b40d176e1f9fc281e95c20
조현지 <qndjddl001@gmail.com>
2024-12-06 00:00:54 +0900

    feat: 유저 별 주문 조회 구현
    user가 보낸 주문 건 조회
    - isActive = false일 경우에도 주문은 조회됨
    - 본인의 주문만 조회 가능

--- src/main/java/com/efactoring/cheesecakefactory/domain/user/controller/UserController.java
+++ src/main/java/com/efactoring/cheesecakefactory/domain/user/controller/UserController.java
@@ -1,6 +1,16 @@
 package com.efactoring.cheesecakefactory.domain.user.controller;
 
-import com.efactoring.cheesecakefactory.domain.user.dto.*;
+import com.efactoring.cheesecakefactory.domain.common.SuccessResponseDto;
+import com.efactoring.cheesecakefactory.domain.order.dto.OrderResponseDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.DeleteUserRequestDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.LoginRequestDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.LoginResponseDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.PatchUserRequestDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.PatchUserResponseDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.SignupRequestDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.SignupResponseDto;
+import com.efactoring.cheesecakefactory.domain.user.dto.UserInfoResponseDto;
+import com.efactoring.cheesecakefactory.domain.user.entity.User;
 import com.efactoring.cheesecakefactory.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -8,8 +18,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -44,7 +63,7 @@ public class UserController {
         }
         session.invalidate();
 
-        return new ResponseEntity<>("로그아웃 성공!",HttpStatus.OK);
+        return new ResponseEntity<>("로그아웃 성공!", HttpStatus.OK);
     }
 
     // 유저정보수정
@@ -69,5 +88,24 @@ public class UserController {
         userService.userStatusChange(id, request, dto.getPassword());
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * 유저별 주문 조회
+     *
+     * @param id user entity id
+     * @return List<OrderResponseDto>
+     * @throws ResponseStatusException forbidden = 본인 주문 아닌 경우 || user 가 아닌경우
+     */
+    @GetMapping("/{id}/orders")
+    public ResponseEntity<SuccessResponseDto> getUserOrders(
+            @PathVariable Long id,
+            @SessionAttribute User user
+    ) {
+        List<OrderResponseDto> orderResponseDtoList = userService.getUserOrder(id, user);
+
+        SuccessResponseDto successResponseDto = new SuccessResponseDto("OK", 200, orderResponseDtoList);
+
+        return new ResponseEntity<>(successResponseDto, HttpStatus.OK);
     }
 }
