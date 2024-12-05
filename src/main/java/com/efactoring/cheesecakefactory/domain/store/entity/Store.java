3da32ff24bd6d614948be5d37290e4da37dfdc57
조현지 <qndjddl001@gmail.com>
2024-12-06 00:30:13 +0900

    update: cascade 추가
    - 가게 삭제시 메뉴, 주문 사라지도록 cascade 추가
    
    [todo]
    - 현재 진행중인 주문건 있을 경우 가게 삭제되지 않도록 예외 처리
    - 이후 리뷰 기능 구현 완료 후 리뷰도 함께 사라지도록 설정
    - 가게 부분 예외처리 전체적으로 추가 필요

--- src/main/java/com/efactoring/cheesecakefactory/domain/store/entity/Store.java
+++ src/main/java/com/efactoring/cheesecakefactory/domain/store/entity/Store.java
@@ -1,19 +1,24 @@
 package com.efactoring.cheesecakefactory.domain.store.entity;
 
 import com.efactoring.cheesecakefactory.domain.base.BaseEntity;
+import com.efactoring.cheesecakefactory.domain.menu.entity.Menu;
 import com.efactoring.cheesecakefactory.domain.user.entity.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -37,4 +42,7 @@ public class Store extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Menu> menu = new ArrayList<>();
 }
