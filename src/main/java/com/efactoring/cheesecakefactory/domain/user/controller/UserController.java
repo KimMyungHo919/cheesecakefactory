e5e5122d121e5ca17766f88ebd70bf6b6cf3b8a5
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2024-12-06 16:29:03 +0900

    refactor: User request매개변수 수정
    튜터님 피드백인 HttpServlet객체 Service단 까지 내려가는 코드 수정완료

--- src/main/java/com/efactoring/cheesecakefactory/domain/user/controller/UserController.java
+++ src/main/java/com/efactoring/cheesecakefactory/domain/user/controller/UserController.java
@@ -40,7 +40,13 @@ public class UserController {
     // 회원가입
     @PostMapping("/signup")
     public ResponseEntity<SignupResponseDto> registerUser(@RequestBody @Valid SignupRequestDto dto, HttpServletRequest request) {
-        SignupResponseDto signupUser = userService.registerUser(dto, request);
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그아웃 먼저 해주세요.");
+        }
+
+        SignupResponseDto signupUser = userService.registerUser(dto);
 
         return new ResponseEntity<>(signupUser, HttpStatus.CREATED);
     }
@@ -48,7 +54,18 @@ public class UserController {
     // 로그인
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto dto, HttpServletRequest request) {
-        LoginResponseDto loginUser = userService.loginUser(dto.getEmail(), dto.getPassword(), request);
+
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("user") != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 로그인된 사용자입니다.");
+        }
+
+        User user = userService.loginUser(dto.getEmail(), dto.getPassword());
+
+        session = request.getSession(true);
+        session.setAttribute("user", user);
+
+        LoginResponseDto loginUser = new LoginResponseDto(user);
 
         return new ResponseEntity<>(loginUser, HttpStatus.OK);
     }
@@ -56,8 +73,8 @@ public class UserController {
     // 로그아웃
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
 
+        HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("user") == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 로그아웃된 사용자입니다.");
         }
@@ -69,7 +86,18 @@ public class UserController {
     // 유저정보수정
     @PatchMapping("/{id}")
     public ResponseEntity<PatchUserResponseDto> patchUser(@PathVariable Long id, @RequestBody @Valid PatchUserRequestDto dto, HttpServletRequest request) {
-        PatchUserResponseDto patchUser = userService.patchUser(id, dto, request);
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인을 먼저 해주세요.");
+        }
+
+        User loginUser = (User) session.getAttribute("user");
+        if (!loginUser.getId().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "자신의 정보만 수정가능합니다. 아이디를 확인해주세요.");
+        }
+
+        PatchUserResponseDto patchUser = userService.patchUser(id, dto);
 
         return new ResponseEntity<>(patchUser, HttpStatus.OK);
     }
@@ -77,7 +105,18 @@ public class UserController {
     // id 정보조회
     @GetMapping("/{id}")
     public ResponseEntity<UserInfoResponseDto> getUserInfo(@PathVariable Long id, HttpServletRequest request) {
-        UserInfoResponseDto userInfo = userService.getUserInfo(id, request);
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인을 먼저 해주세요.");
+        }
+
+        User loginUser = (User) session.getAttribute("user");
+        if (!loginUser.getId().equals(id)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "자신의 정보만 조회가능합니다. 아이디를 확인해주세요.");
+        }
+
+        UserInfoResponseDto userInfo = userService.getUserInfo(id);
 
         return new ResponseEntity<>(userInfo, HttpStatus.OK);
     }
@@ -85,7 +124,15 @@ public class UserController {
     // 회원탈퇴
     @PatchMapping("/{id}/status")
     public ResponseEntity<Void> userDelete(@PathVariable Long id, HttpServletRequest request, @RequestBody DeleteUserRequestDto dto) {
-        userService.userStatusChange(id, request, dto.getPassword());
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인을 먼저 해주세요.");
+        }
+
+        userService.userStatusChange(id, dto.getPassword());
+
+        session.invalidate();
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/com/efactoring/cheesecakefactory/domain/user/service/UserService.java
+++ src/main/java/com/efactoring/cheesecakefactory/domain/user/service/UserService.java
@@ -32,12 +32,7 @@ public class UserService {
     private final OrderRepository orderRepository;
 
     // 유저 회원가입
-    public SignupResponseDto registerUser(SignupRequestDto dto, HttpServletRequest request) {
-
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그아웃 먼저 해주세요.");
-        }
+    public SignupResponseDto registerUser(SignupRequestDto dto) {
 
         if (userRepository.existsByEmail(dto.getEmail())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.");
@@ -61,14 +56,10 @@ public class UserService {
     }
 
     // 유저 로그인
-    public LoginResponseDto loginUser(String email, String password, HttpServletRequest request) {
+    public User loginUser(String email, String password) {
+
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 이메일입니다."));
-
-        HttpSession session = request.getSession(false);
-        if (session != null && session.getAttribute("user") != null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 로그인된 사용자입니다.");
-        }
 
         if (user.getStatus().equals("Deleted")) {
             throw new IllegalArgumentException("탈퇴한 회원입니다.");
@@ -78,25 +69,13 @@ public class UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호를 다시 확인해주세요.");
         }
 
-        session = request.getSession(true);
-        session.setAttribute("user", user);
-
-        return new LoginResponseDto(user);
+        return user;
     }
 
     // 유저 정보수정
-    public PatchUserResponseDto patchUser(Long id, PatchUserRequestDto dto, HttpServletRequest request) {
+    public PatchUserResponseDto patchUser(Long id, PatchUserRequestDto dto) {
+
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("없는아이디"));
-
-        HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("user") == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인을 먼저 해주세요.");
-        }
-        User loginUser = (User) session.getAttribute("user");
-
-        if (!user.getEmail().equals(loginUser.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "본인정보만 수정가능합니다.");
-        }
 
         if (!passwordEncoder.matches(dto.getOldPassword(), user.getPassword())) { // 앞에가 평문비밀번호, 뒤에가 암호화비밀번호
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호를 다시 확인해주세요.");
@@ -115,47 +94,27 @@ public class UserService {
     }
 
     // 유저 정보조회. 자신만가능.
-    public UserInfoResponseDto getUserInfo(Long id, HttpServletRequest request) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당아이디없음"));
-        HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("user") == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인을 먼저 해주세요.");
-        }
-        User loginUser = (User) session.getAttribute("user");
+    public UserInfoResponseDto getUserInfo(Long id) {
 
-        if (!user.getEmail().equals(loginUser.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "본인정보만 조회 가능합니다.");
-        }
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당아이디없음"));
 
         return new UserInfoResponseDto(user);
     }
 
     // 유저탈퇴
-    public void userStatusChange(Long id, HttpServletRequest request, String password) {
+    public void userStatusChange(Long id, String password) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("일치하는 유저없음"));
 
-        HttpSession session = request.getSession(false);
-
-        if (session == null || session.getAttribute("user") == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인을 먼저 해주세요.");
-        }
-        User loginUser = (User) session.getAttribute("user");
-
-        if (!passwordEncoder.matches(password, loginUser.getPassword())) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호를 다시 확인해주세요.");
         }
 
-        if (!user.getEmail().equals(loginUser.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "본인만 탈퇴 가능합니다.");
-        }
-
         if ("Deleted".equals(user.getStatus())) {
-            throw new IllegalArgumentException("이미 탈퇴상태인 유저입니다.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "이미 탈퇴한 유저입니다.");
         }
 
         user.setStatus("Deleted");
         userRepository.save(user);
-        session.invalidate();
     }
 
     public List<OrderResponseDto> getUserOrder(Long id, User user) {
